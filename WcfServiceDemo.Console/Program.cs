301388a00f10a5cc195e55a11c65bc4fe6b2a1c7
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2021-06-17 00:57:46 +0200

    Add project files.

---
+++ WcfServiceDemo.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using WcfServiceDemo.Console.ServiceReference;
+
+namespace WcfServiceDemo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IpAddressServiceClient client = new IpAddressServiceClient();
+            var ipAddress = client.GetIpAddress();
+            System.Console.WriteLine(ipAddress.IpAddress);
+            System.Console.ReadLine();
+        }
+    }
+}
